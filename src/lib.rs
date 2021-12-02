84eecb2ac860033bd05989136c23ff9d670ede99
Saniya Maheshwari <saniya.mah@gmail.com>
2021-12-02 17:35:10 +0530

    Better error handling
    Also applied clippy and rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use std::collections::{HashSet, HashMap};
 use ndarray::prelude::*;
 use ndarray_linalg::*;
 use plotters::coord::types::RangedCoordf32;
 use plotters::prelude::*;
+use std::collections::{HashMap, HashSet};
 
 pub struct TextExample {
     docs: [&'static str; 9],
@@ -10,13 +10,11 @@ pub struct TextExample {
     vocab: Vec<&'static str>,
     count_matrix: Array<f64, Ix2>,
     doc_vecs: Array<f64, Ix2>,
-    term_vecs: Array<f64, Ix2>
+    term_vecs: Array<f64, Ix2>,
 }
 
 impl TextExample {
-
     pub fn setup(dim: usize) -> TextExample {
-
         let docs = [
             "human machine interface for lab ABC computer applications",
             "a survey of user opinion of computer system response time",
@@ -26,18 +24,16 @@ impl TextExample {
             "the generation of random binary unordered trees",
             "the intersection graph of paths in trees",
             "graph minors widths of trees and well quasi ordering",
-            "graph minors a survey"
+            "graph minors a survey",
         ];
 
-        let doc_labels = [
-            "h1", "h2", "h3", "h4", "h5", "g1", "g2", "g3", "g4"
-        ];
+        let doc_labels = ["h1", "h2", "h3", "h4", "h5", "g1", "g2", "g3", "g4"];
 
         // prepare the vocab
         let mut vocab = HashSet::new();
 
         for text in &docs {
-            for token in text.split(' ') {
+            for token in text.split_whitespace() {
                 vocab.insert(token);
             }
         }
@@ -45,26 +41,22 @@ impl TextExample {
         // map tokens from words to indices in the vocab
         let mut vocab_to_idx: HashMap<&str, usize> = HashMap::new();
 
-        let mut i = 0;
-        for v in vocab.iter() {
+        for (i, v) in vocab.iter().enumerate() {
             vocab_to_idx.insert(v, i);
-            i += 1;
         }
 
-        // initialize the count matrix
+        // prepare the count matrix
         let mut count_matrix: Array<usize, Ix2> = Array::zeros((vocab.len(), docs.len()));
 
         for (d, doc) in docs.iter().enumerate() {
-            for token in doc.split(' ') {
-
-                let i = match vocab_to_idx.get(token) {
+            for token in doc.split_whitespace() {
+                let i = *match vocab_to_idx.get(token) {
                     None => panic!("token not in vocab!"),
-                    Some(i) => i.clone()
+                    Some(i) => i,
                 };
 
-                // increment the count value by one
                 let val = count_matrix[[i, d]];
-                count_matrix.slice_mut(s![i, d]).fill(val + 1 as usize);
+                count_matrix.slice_mut(s![i, d]).fill(val + 1_usize);
             }
         }
 
@@ -73,8 +65,8 @@ impl TextExample {
         // also remove stopwords: 'and', 'of', 'the', 'a'.
         vocab_to_idx = vocab_to_idx
             .into_iter()
-            .filter(|(_, i)| count_matrix.slice(s![i.clone(), ..]).sum() > 1)
-            .filter(|(v, _)| ! ["and", "of", "the", "a"].contains(v))
+            .filter(|(_, i)| count_matrix.slice(s![*i, ..]).sum() > 1)
+            .filter(|(v, _)| !["and", "of", "the", "a"].contains(v))
             .collect();
 
         // update the vocabulary and count matrix accordingly
@@ -89,51 +81,48 @@ impl TextExample {
 
         // normalize the count matrix by the number of tokens occurring in
         // each document.
-        let count_matrix = count_matrix.mapv(|c| c as f64) /
-            &count_matrix
-            .sum_axis(Axis(0))
-            .insert_axis(Axis(0))
-            .mapv(|c| c as f64);
+        let count_matrix = count_matrix.mapv(|c| c as f64)
+            / &count_matrix
+                .sum_axis(Axis(0))
+                .insert_axis(Axis(0))
+                .mapv(|c| c as f64);
 
         // take SVD
-        let (u_matr, sing, vt_matr) = TruncatedSvd::new(count_matrix.clone(), TruncatedOrder::Largest)
-            .decompose(dim)
-            .unwrap()
-            .values_vectors();
-
-        // convert the singular values to a diagonal matrix
-        let sing: Array<f64, Ix2> = Array::from_diag(&sing);
+        let (u_matr, sing, vt_matr) =
+            TruncatedSvd::new(count_matrix.clone(), TruncatedOrder::Largest)
+                .decompose(dim)
+                .expect("Failed to take SVD.")
+                .values_vectors();
 
         // get term and document vectors
+        let sing: Array<f64, Ix2> = Array::from_diag(&sing);
         let term_vecs = u_matr.dot(&sing);
         let doc_vecs = sing.dot(&vt_matr).reversed_axes();
 
-
         TextExample {
-            docs: docs,
-            doc_labels: doc_labels,
-            vocab: vocab,
-            count_matrix: count_matrix,
-            doc_vecs: doc_vecs,
-            term_vecs: term_vecs
+            docs,
+            doc_labels,
+            vocab,
+            count_matrix,
+            doc_vecs,
+            term_vecs,
         }
     }
 
     fn display_docs(&self) {
-
         // maximum length of document labels
         let maxlen = match self.doc_labels.iter().max_by_key(|l| l.len()) {
             None => panic!("Cannot find maximum."),
-            Some(i) => i
-        }.len();
+            Some(i) => i,
+        }
+        .len();
 
         for (label, doc) in self.doc_labels.iter().zip(self.docs) {
-            println!("{:w$}: {}", label, doc, w=maxlen);
+            println!("{:w$}: {}", label, doc, w = maxlen);
         }
     }
 
     fn display_count_matrix(&self) {
-
         // all labels involved - document labels and term names
         let mut all_labels = Vec::from(self.doc_labels);
         for v in self.vocab.iter() {
@@ -143,102 +132,90 @@ impl TextExample {
         // maximum length of labels
         let maxlen = match all_labels.into_iter().max_by_key(|l| l.len()) {
             None => panic!("Cannot find maximum."),
-            Some(i) => i
-        }.len();
+            Some(i) => i,
+        }
+        .len();
 
         // formatting width
-        let width = if maxlen > 4 {maxlen} else {4};
+        let width = if maxlen > 4 { maxlen } else { 4 };
 
-        // printing spaces for formatting
-        for _ in 0..width+1 {
+        for _ in 0..width + 1 {
             print!(" ");
         }
 
-        // print the term names
         for term in self.vocab.iter() {
-            print!("{:>w$} ", term, w=width);
+            print!("{:>w$} ", term, w = width);
         }
-        print!("\n");
+        println!();
 
         for (d, label) in self.doc_labels.iter().enumerate() {
+            print!("{:w$} ", label, w = width);
 
-            // print the document label
-            print!("{:w$} ", label, w=width);
-
-            // print the count values
             for i in 0..self.vocab.len() {
-                print!("{:w$.2} ", self.count_matrix[[i, d]], w=width);
+                print!("{:w$.2} ", self.count_matrix[[i, d]], w = width);
             }
 
-            print!("\n");
+            println!();
         }
     }
 
     fn display_vecs(vecs: &Array<f64, Ix2>, labels: &[&str]) {
-
         // maximum length of the labels
         let maxlen = match labels.iter().max_by_key(|l| l.len()) {
             None => panic!("Cannot find maximum."),
-            Some(i) => i
-        }.len();
+            Some(i) => i,
+        }
+        .len();
 
         // formatting width
-        let width = if maxlen > 7 {maxlen} else {7};
+        let width = if maxlen > 7 { maxlen } else { 7 };
 
-        // print the vector elements
         for (i, l) in labels.iter().enumerate() {
-            println!("{:w$} {:+w$.4}", l, vecs.slice(s![i, ..]), w=width);
+            println!("{:w$} {:+w$.4}", l, vecs.slice(s![i, ..]), w = width);
         }
     }
 
     fn display_sim_matrix(vecs: &Array<f64, Ix2>, labels: &[&str]) {
-
-        // get l2-norms of all vectors
+        // normalize and calculate the similarity matrix
         let norm_vals = vecs
             .mapv(|v| v.powi(2))
             .sum_axis(Axis(1))
             .mapv(f64::sqrt)
             .insert_axis(Axis(1));
-
-        // normalize and calculate the similarity matrix
         let norm_vecs = vecs / &norm_vals;
         let sim_matrix = norm_vecs.dot(&norm_vecs.t());
 
         // maximum length of the labels
         let maxlen = match labels.iter().max_by_key(|l| l.len()) {
-            None => panic!("Could not find maximum."),
-            Some(i) => i
-        }.len();
+            None => panic!("Failed to find maximum."),
+            Some(i) => i,
+        }
+        .len();
 
         // formatting width
-        let width = if maxlen > 7 {maxlen} else {7};
+        let width = if maxlen > 7 { maxlen } else { 7 };
 
-        // printing spaces for formatting
-        for _ in 0..width+1 {
+        for _ in 0..width + 1 {
             print!(" ");
         }
 
-        // print column labels
         for l in labels.iter() {
-            print!("{:>w$} ", l, w=width);
+            print!("{:>w$} ", l, w = width);
         }
-        print!("\n");
+        println!();
 
         for (i, l) in labels.iter().enumerate() {
+            print!("{:w$} ", l, w = width);
 
-            // print row label
-            print!("{:w$} ", l, w=width);
-
-            // print similarity values
             for j in 0..labels.len() {
-                print!("{:w$.4} ", sim_matrix[[i, j]], w=width);
+                print!("{:w$.4} ", sim_matrix[[i, j]], w = width);
             }
 
-            print!("\n");
+            println!();
         }
     }
 
-    pub fn display(&self, key: &str) {
+    pub fn display(&self, key: &str) -> Result<(), String> {
         match key {
             "docs" => self.display_docs(),
             "count_matrix" => self.display_count_matrix(),
@@ -246,41 +223,63 @@ impl TextExample {
             "doc_vecs" => TextExample::display_vecs(&self.doc_vecs, &self.doc_labels),
             "term_sim" => TextExample::display_sim_matrix(&self.term_vecs, &self.vocab),
             "doc_sim" => TextExample::display_sim_matrix(&self.doc_vecs, &self.doc_labels),
-            _ => panic!("Invalid key!")
+            _ => {
+                return Err(format!(
+                    "Invalid key '{}'! Key must be one of 'docs', 'count_matrix', \
+                    'term_vecs', 'doc_vecs', 'term_sim' and 'doc_sim'.",
+                    key
+                ))
+            }
         };
+
+        Ok(())
     }
 
-    pub fn plot(&self, key: &str) {
-
+    pub fn plot(&self, key: &str) -> Result<(), String> {
         // vec to store doc_labels which is an array
         let doc_labels = Vec::from(self.doc_labels);
 
         let (vecs, labels) = match key {
             "term_vecs" => (&self.term_vecs, &self.vocab),
             "doc_vecs" => (&self.doc_vecs, &doc_labels),
-            _ => panic!("Invalid key!")
+            _ => {
+                return Err(format!(
+                    "Invalid key '{}'! Key must be one of 'term_vecs' and 'doc_vecs'.",
+                    key
+                ))
+            }
         };
 
-        assert_eq!(vecs.len_of(Axis(1)), 2, "Vectors need to be 2-dimensional to plot!");
+        let dim = vecs.len_of(Axis(1));
+        if dim != 2 {
+            return Err(format!(
+                "Vectors are of dimension {}! They need to be 2-dimensional to plot!",
+                dim
+            ));
+        }
 
         let filename = format!("{}_plot.svg", key);
 
         // limits of x- and y-axes
-        let x_lim = match vecs
-            .slice(s![.., 0])
-            .iter()
-            .max_by(|a, b| a.abs().partial_cmp(&b.abs()).unwrap()) {
-                None => panic!("Could not find maximum."),
-                Some(i) => i.clone()
-            }.abs() as f32;
+        let x_lim = (*match vecs.slice(s![.., 0]).iter().max_by(|a, b| {
+            a.abs()
+                .partial_cmp(&b.abs())
+                .expect("Failed to compare f64 values.")
+        }) {
+            None => panic!("Failed to find maximum."),
+            Some(i) => i,
+        })
+        .abs() as f32;
 
-        let y_lim = match vecs
-            .slice(s![.., 1])
-            .iter()
-            .max_by(|a, b| a.abs().partial_cmp(&b.abs()).unwrap()) {
-                None => panic!("Could not find maximum."),
-                Some(i) => i.clone()
-            }.abs() as f32;
+        let y_lim = (*match vecs.slice(s![.., 1]).iter().max_by(|a, b| {
+            a.abs()
+                .partial_cmp(&b.abs())
+                .expect("Failed to compare f64 values.")
+        }) {
+            None => panic!("Failed to find maximum."),
+            Some(i) => i,
+        })
+        .abs() as f32;
 
         // size in pixels of x- and y-dimensions
         const BASE_SIZE: u32 = 400;
@@ -289,43 +288,39 @@ impl TextExample {
         let mut x_size = BASE_SIZE;
 
         // adjusting size of y-dimension to maintain aspect ratio
-        let mut y_size = (x_size as f64 +
-            ((y_lim/x_lim) as f64 - 1.0) * x_size as f64
-        ) as u32;
+        let mut y_size = (x_size as f64 + ((y_lim / x_lim) as f64 - 1.0) * x_size as f64) as u32;
 
         // setting up coordinate system
         let coord: Cartesian2d<RangedCoordf32, RangedCoordf32> = Cartesian2d::new(
             -x_lim..x_lim,
             -y_lim..y_lim,
-            (0..x_size as i32, 0..y_size as i32)
+            (0..x_size as i32, 0..y_size as i32),
         );
 
         // adding padding to account for any text spillovers
-        x_size = x_size + PADDING;
-        y_size = y_size + PADDING;
+        x_size += PADDING;
+        y_size += PADDING;
 
         // create figure
         let root = SVGBackend::new(&filename, (x_size, y_size))
             .into_drawing_area()
             .apply_coord_spec(coord);
+        root.fill(&WHITE).expect("Failed to fill figure.");
 
-        // fill with white background colour
-        root.fill(&WHITE).unwrap();
-
-        // function to draw a dot with a label
         let dot_and_label = |x: f32, y: f32, label: &'static str| {
             EmptyElement::at((x, y))
                 + Circle::new((0, 0), 3, ShapeStyle::from(&BLACK).filled())
-                + Text::new(
-                    label, (5, 0), ("sans-serif", 15.0).into_font()
-                )
+                + Text::new(label, (5, 0), ("sans-serif", 15.0).into_font())
         };
 
         // plot vectors
         for (row, label) in vecs.axis_iter(Axis(0)).zip(labels) {
             let row = row.into_owned().into_raw_vec();
-            let (x, y) = ((&row[0]).clone() as f32, (&row[1]).clone() as f32);
-            root.draw(&dot_and_label(x, y, label)).unwrap();
+            let (x, y) = (row[0] as f32, row[1] as f32);
+            root.draw(&dot_and_label(x, y, label))
+                .expect("Failed to draw on figure.");
         }
+
+        Ok(())
     }
 }
