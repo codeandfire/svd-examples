f38ecf1af8751f5a5a088c95153114ad7f781b11
Saniya Maheshwari <saniya.mah@gmail.com>
2021-12-12 18:19:08 +0530

    First draft of main script with only image example
    Command-line arguments have been temporarily removed

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,22 @@
-use clap::Parser;
-use svd_examples::LsaExample;
+use image::io::Reader as ImageReader;
+use image::GrayImage;
+use svd_examples::*;
 
-#[derive(Parser)]
-#[clap(about = "Examples of the SVD.")]
-struct Opts {
-    #[clap(subcommand)]
-    subcmd: SubCommand,
-}
+fn image_example() {
+    let img = ImageReader::open("face.png").unwrap().decode().unwrap();
+    let img = img.into_luma8();
+    let arr = GrayPixelArray::from(img);
+    let arr = arr.mapv(|v| v as f64);
 
-#[derive(Parser)]
-enum SubCommand {
-    Lsa(LsaOpts),
-}
-
-#[derive(Parser)]
-#[clap(about = "Latent Semantic Analysis (LSA) example from Deerwester et. al. (1990)")]
-struct LsaOpts {
-    #[clap(
-        short = 'k',
-        long,
-        about = "Number of singular values",
-        default_value = "2"
-    )]
-    dim_k: usize,
-
-    #[clap(long, about = "Display the nine documents")]
-    disp_docs: bool,
-
-    #[clap(long, about = "Display the count matrix")]
-    disp_count: bool,
-
-    #[clap(long, about = "Display document similarity matrix")]
-    disp_doc_sim: bool,
-
-    #[clap(long, about = "Display term similarity matrix")]
-    disp_term_sim: bool,
+    for k in (5..=50).step_by(5) {
+        let (u, s, vt) = arr.take_svd(k);
+        let trunc_arr = u.dot(&s).dot(&vt);
+        let trunc_arr = GrayPixelArray(trunc_arr.mapv(|v| v as u8));
+        let trunc_img = GrayImage::from(trunc_arr);
+        trunc_img.save(format!("k_{}.png", k)).expect("Failed to save image.");
+    }
 }
 
 fn main() {
-    let opts = Opts::parse();
-
-    match opts.subcmd {
-        SubCommand::Lsa(lsa_opts) => {
-            let lsa_example = LsaExample::setup(lsa_opts.dim_k);
-
-            if lsa_opts.disp_docs {
-                println!("Nine documents:");
-                lsa_example.display("docs").unwrap();
-                println!();
-            }
-
-            if lsa_opts.disp_count {
-                println!("Count matrix:");
-                lsa_example.display("count_matrix").unwrap();
-                println!();
-            }
-
-            if lsa_opts.disp_doc_sim {
-                println!("Document similarity matrix:");
-                lsa_example.display("doc_sim").unwrap();
-                println!();
-            }
-
-            if lsa_opts.disp_term_sim {
-                println!("Term similarity matrix:");
-                lsa_example.display("term_sim").unwrap();
-                println!();
-            }
-
-            println!("Document vectors:");
-            lsa_example.display("doc_vecs").unwrap();
-            lsa_example
-                .plot("doc_vecs", "doc_vecs_plot.svg")
-                .unwrap_or_else(|error| {
-                    println!("Skipping plot: {}", error);
-                });
-            println!();
-
-            println!("Term vectors:");
-            lsa_example.display("term_vecs").unwrap();
-            lsa_example
-                .plot("term_vecs", "term_vecs_plot.svg")
-                .unwrap_or_else(|error| {
-                    println!("Skipping plot: {}", error);
-                });
-            println!();
-        }
-    }
+    image_example();
 }
